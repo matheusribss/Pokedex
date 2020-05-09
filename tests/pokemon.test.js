11d3efec32b471444985bbd231cf6c138b665ca0
matheusribss <matheus.ribs.sjc@gmail.com>
2020-05-09 17:15:36 -0300

    teste da classe pokemon feita (#5)

---
+++ tests/pokemon.test.js
@@ -0,0 +1,34 @@
+const Pokemon = require("../src/pokemon") 
+
+const esboco= new Pokemon ("agahmenon", 10)
+
+const esboco2= new Pokemon ( undefined,10)
+
+
+
+test ("deveria criar um pokemon",()=>{
+
+    
+    expect (esboco.vida).toBe(100)
+    expect (esboco.estaVivo).toBeTruthy()
+    expect (esboco2.nome).toBeUndefined()
+
+})
+
+
+
+test ("deveria usar poder", ()=>{
+
+    expect (esboco.usarPoder("paulada")).toBe(100)
+    
+    expect (esboco.usarPoder(null)).toBeNull()
+
+    esboco.vida = 0
+    expect (esboco.usarPoder("paulada")).toBeNull()
+})
+
+
+test ("deveria ver status", ()=>{
+
+    expect (esboco.verStatus()).not.toBeUndefined()
+})
